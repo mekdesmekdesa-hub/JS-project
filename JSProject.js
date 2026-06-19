c12e12313c27d36b207e3605cf4aa64eceb63fe8
mekdesmekdesa.hub <mekdesmekdesa@gmail.com>
2026-06-19 17:59:33 +0300

    keyboard input linked

--- JSProject.js
+++ JSProject.js
@@ -17,11 +17,29 @@ drawnigPen.fillStyle="black";
 drawnigPen.arc(62,58,4,0, Math.PI*2);
 drawnigPen.fill();
 //movement 
-let x=50;
+let x=10;
 let y=50;
+//connecting keyboard keys to the JS
+let direction="RIGHT";
+document.addEventListener("keydown",handleKeys);
+function handleKeys(event){
+    alert("wow")
+    if (event.key=="ArrowUp"){
+        direction="UP";
+    }
+    else if(event.key=="ArrowDown"){
+        direction="DOWN";
+    }
+    else if(event.key=="ArrowLeft"){
+        direction="LEFT";
+    }
+    else if(event.key=="ArrowRight"){
+        direction="RIGHT";
+    }
+}
 function update(){
-    if(x>=boardWidth-20){
-        x=50;
+    if(x>=boardWidth-20){//wall collision
+        x=x;
     }
     else {
         x=x+1;
@@ -31,6 +49,12 @@ function draw(){
     drawnigPen.clearRect(0,0,canvas.width,canvas.height);
     drawnigPen.fillStyle="pink";
     drawnigPen.fillRect(x,y,20,20);
+    drawnigPen.lineWidth=20;
+    const offSet=drawnigPen.lineWidth/2;
+    drawnigPen.strokeStyle="blue";
+    const boardWidth=canvas.width-drawnigPen.lineWidth;
+    const boardHeight=canvas.height-drawnigPen.lineWidth;
+    +drawnigPen.strokeRect(offSet,offSet,boardWidth,boardHeight);
 }
 function move(){
     update();
